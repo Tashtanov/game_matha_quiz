83ca70b484dfd89f4de0c21773b5de73d3c6067c
Tashtanov <tashtanov333@gmail.com>
2022-06-15 09:21:24 +0600

    git initialization

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'homescreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Snake Xenzia',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home:  HomeScreen(),
+    );
+  }
+}
+
+
