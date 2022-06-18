e4649a78cd379752199586b3d2dadfd2f5309865
Tashtanov <tashtanov333@gmail.com>
2022-06-19 03:44:34 +0600

    true false buttons

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -10,13 +10,25 @@ const kColorizeAnimationColors = [
   Colors.green,
 ];
 const kAnimationTextStyle = TextStyle(
-  fontSize: 50,
-  fontWeight: FontWeight.bold,
-  fontFamily: "Lobster"
+    fontSize: 50,
+    fontWeight: FontWeight.bold,
+    fontFamily: "Lobster"
 );
 const kTapToStartTextStyle = TextStyle(
   fontSize: 25,
   color: Colors.black,
 );
-const kScoreTextIndicator =  TextStyle(fontFamily: "Press Start 2P",fontSize: 20,color: Colors.yellow);
-const kScoreIndicator =  TextStyle(fontFamily: "Chakra Petch",fontSize: 25,fontWeight: FontWeight.bold);
+const kScoreTextIndicator = TextStyle(
+    fontFamily: "Press Start 2P", fontSize: 20, color: Colors.yellow);
+const kScoreIndicator = TextStyle(
+    fontFamily: "Chakra Petch", fontSize: 25, fontWeight: FontWeight.bold);
+const kQuizTextbody = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 30,
+    fontFamily: "Architects Daughter",
+    color: Colors.white
+);
+const kGradientcolor= [Colors.blueAccent, Colors.lightBlueAccent];
+const kTextButtonStyleTrueFalse=TextStyle(
+    fontSize: 25, );
+
--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:game_app/common/constants.dart';
 import 'package:game_app/widgets/scorea_indicators.dart';
 import 'package:game_app/quiz_game.dart';
+import 'package:outline_gradient_button/outline_gradient_button.dart';
 
 QuizBrain _quizBrain = QuizBrain();
-
+ int _score = 0;
+ int _highScore = 0;
 class GameScreen extends StatefulWidget {
   static final id = "game_screen";
 
@@ -32,29 +35,68 @@ class _GameScreenState extends State<GameScreen> {
           height: double.infinity,
           decoration: BoxDecoration(
               gradient: LinearGradient(
-            colors: [Colors.blueAccent, Colors.lightBlueAccent],
+            colors: kGradientcolor,
           )),
           child: Column(
             children: [
               ScoreIndicators(),
-              Expanded(
-                  child: FittedBox(
-                      child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Text(
-                  _quizBrain.quiz,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                      fontFamily: "Architects Daughter",
-                    color: Colors.white
-                  ),
-                ),
-              ))),
+              GameBody(),
+              Row(
+                children: [
+                  ReUsableOutLinedButton(textName: "FALSE",colorName: Colors.redAccent,),
+                  ReUsableOutLinedButton(textName: "TRUE",colorName: Colors.lightGreenAccent,),
+
+                ],
+              )
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class ReUsableOutLinedButton extends StatelessWidget {
+  final String textName;
+  final Color colorName;
+  const ReUsableOutLinedButton({
+    Key? key, required this.textName,required this.colorName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: OutlineGradientButton(
+          padding: EdgeInsets.all(20),
+            elevation: 1,
+            child: Center(
+                child: Text(
+              textName,
+              style: kTextButtonStyleTrueFalse.copyWith(color: colorName),
+            )),
+            strokeWidth: 12,
+            radius: Radius.circular(40),
+            onTap: () {},
+            gradient: LinearGradient(
+                colors:kGradientcolor)),
+      ),
+    );
+  }
+}
+
+class GameBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: FittedBox(
+            child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Text(
+        _quizBrain.quiz,
+        style: kQuizTextbody,
+      ),
+    )));
   }
 }
